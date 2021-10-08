a00103a9e6849f316fb336f6a6773bed461e3b13
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-08 20:06:32 -0300

    adiciona exercicios 7

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -77,28 +77,59 @@
 
 // 6 - Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 
-let peca = 'torre';
-let pecaLower = peca.toLocaleLowerCase();
+// let peca = 'tore';
+// let pecaLower = peca.toLocaleLowerCase();
 
-switch (pecaLower) {
-  case 'peao':
-    console.log('para frente');
-    break;
-  case 'torre':
-    console.log('horizontal e vertical');
-    break;
-  case 'bispo':
-    console.log('diagonal');
-    break;
-  case 'cavalo':
-    console.log('em L');
-    break;
-  case 'rainha':
-    console.log('todas as direções')
-    break;
-  case 'rei':
-    console.log('vertical e horizontal');
-    break;          
-  default:
-    console.log('Inválido');  
+// switch (pecaLower) {
+//   case 'peao':
+//     console.log('para frente');
+//     break;
+//   case 'torre':
+//     console.log('horizontal e vertical');
+//     break;
+//   case 'bispo':
+//     console.log('diagonal');
+//     break;
+//   case 'cavalo':
+//     console.log('em L');
+//     break;
+//   case 'rainha':
+//     console.log('todas as direções')
+//     break;
+//   case 'rei':
+//     console.log('vertical e horizontal');
+//     break;          
+//   default:
+//     console.log('Inválido, não existe esta peça :(');  
+// }
+
+// 7 - Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+
+// Porcentagem >= 90 -> A
+// Porcentagem >= 80 -> B
+// Porcentagem >= 70 -> C
+// Porcentagem >= 60 -> D
+// Porcentagem >= 50 -> E
+// Porcentagem < 50 -> F
+
+// O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+
+let nota = -1;
+
+if(nota < 0 || nota > 100) {
+  console.log('Erro, valor inválido :(');
+}else{
+  if(nota >= 90) {
+    console.log('A');
+  }else if(nota >= 80) {
+    console.log('B');
+  }else if(nota >= 70) {
+    console.log('C');
+  }else if(nota >= 60) {
+    console.log('D');
+  }else if(nota >= 50) {
+    console.log('E');
+  }else{
+    console.log('F');
+  }
 }
