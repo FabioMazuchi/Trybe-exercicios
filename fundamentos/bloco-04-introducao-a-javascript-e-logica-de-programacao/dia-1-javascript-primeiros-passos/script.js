d98d7bc936c671712d36c0cb3aefdd10e0b12b9d
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-08 18:08:06 -0300

    adiciona exercicios 1 ao 3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,53 @@
+// 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+
+// const a = 13;
+// const b = 7;
+
+// Adição
+// let adicao = a + b;
+// console.log(adicao);
+
+// Subtração
+// let subtracao = a - b;
+// console.log(subtracao);
+
+// Multiplicação
+// let multiplicacao = a * b;
+// console.log(multiplicacao);
+
+// Divisão
+// let divisao = a / b;
+// console.log(divisao);
+
+// Módulo
+// let modulo = a % b;
+// console.log(modulo);
+
+// 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+// const n1 = 10;
+// const n2 = 9;
+
+// if(n1 > n2) {
+//     console.log(n1);
+// }else{
+//     console.log(n2);
+// }
+
+// 3 - Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+
+// const n1 = 43;
+// const n2 = 34;
+// const n3 = 133;
+
+// if(n1 > n2 && n1 > n3) {
+//     console.log(n1);
+// }else if(n2 > n1 && n2 > n3){
+//     console.log(n2)
+// }else{
+//     console.log(n3);
+// }
+
+
+
+
