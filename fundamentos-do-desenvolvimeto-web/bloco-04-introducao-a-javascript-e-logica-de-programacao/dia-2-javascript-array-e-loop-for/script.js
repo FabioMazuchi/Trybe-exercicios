07f67188c437743ad6d13692576fa47d81c2ecb0
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-11 20:25:31 -0300

    finaliza exercicios bonus 1 ao 3

--- fundamentos-do-desenvolvimeto-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos-do-desenvolvimeto-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -100,28 +100,60 @@
 // }
 
 
-// /////// EXERCÍCIO 09 ////////
-for (let i = 1; i <= 25; i += 1) {
-  console.log(i / 2);
+// // /////// EXERCÍCIO 09 ////////
+// for (let i = 1; i <= 25; i += 1) {
+//   console.log(i / 2);
+// }
+
+
+// ============ BÔNUS ============
+
+/////// EXERCÍCIO 01 ////////
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] < numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//     }
+//   }
+// }
+
+// console.log(numbers);
+
+
+/////// EXERCÍCIO 02 ////////
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] > numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//     }
+//   }
+// }
+
+// console.log(numbers);
+
+
+/////// EXERCÍCIO 03 ////////
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let multiplica = [];
+let ultimo = numbers.length - 1;
+
+for (let index = 0; index < numbers.length; index += 1) {
+	let n;
+	if (numbers[index] == numbers[ultimo]) {
+		n = numbers[index] * 2;
+		multiplica.push(n);
+	} else {
+		n = numbers[index] * numbers[index + 1];
+		multiplica.push(n);
+	}
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(multiplica);
