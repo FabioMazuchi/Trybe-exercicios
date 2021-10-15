49c388b73a5edd669ebbe1bb1f4660fb746d0512
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-15 18:42:07 -0300

    adiciona exercicio 2

--- fundamentos-do-desenvolvimeto-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/exercicio_1.js
+++ fundamentos-do-desenvolvimeto-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios/exercicio_1.js
@@ -1,7 +1,5 @@
 /*
 
-Usando o objeto abaixo, faça os exercícios a seguir:
-
 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
 
 Bem-vinda, Margarida
