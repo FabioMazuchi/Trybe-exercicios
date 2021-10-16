72905ed1a20a8a55e7514da7ad9b1d1845106c80
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-16 17:54:21 -0300

    adiciona exercicio

--- fundamentos-do-desenvolvimeto-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmo/exercicios/exercicio_5.js
+++ fundamentos-do-desenvolvimeto-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmo/exercicios/exercicio_5.js
@@ -5,13 +5,35 @@
 - Por último, façamos com que a variável seja incrementada com o valor correspondente a cada loop;
 
 n = 7
-
-   *
-  * *
- *   *
+                                 012*456
+   *                             01* *56
+  * *                            0*   *6
+ *   *                           *******
 *******
 
 */
+
+let n = 5;
+let string = '*';
+let linha = '';
+let meioMatriz = (n + 1) / 2;
+let controleEsq = meioMatriz;       	//coluna: 0
+let controleDir = meioMatriz;			//controleDir: 3	
+
+console.log(meioMatriz);
+for (let i = 0; i <= meioMatriz; i++) {
+	for (let coluna = 0; coluna <= n; coluna++) {
+		if (coluna > controleEsq && coluna < controleDir && coluna == controleEsq-coluna) {
+			linha += string;
+		} else {
+			linha += ' ';
+		}
+	}
+	console.log(linha);
+	linha = '';
+	controleEsq--;
+	controleDir++;
+}
 
 
 
