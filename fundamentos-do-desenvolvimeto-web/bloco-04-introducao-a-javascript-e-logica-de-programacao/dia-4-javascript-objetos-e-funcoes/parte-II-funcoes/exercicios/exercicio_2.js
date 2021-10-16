86de02331574d3e8a2bc3ab0ad5e8af71df5106f
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-15 22:00:17 -0300

    adiciona exercicio 2 de funcoes

---
+++ fundamentos-do-desenvolvimeto-web/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/parte-II-funcoes/exercicios/exercicio_2.js
@@ -0,0 +1,25 @@
+/*
+
+2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+- Array de teste: [2, 3, 6, 7, 10, 1]; .
+
+- Valor esperado no retorno da função: 4 .
+
+*/
+
+console.log(maior([2, 3, 6, 7, 10, 1]))
+
+function maior(array) {
+	let maior = array[0];
+	let posicao;
+
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] > maior) {
+			maior = array[i];
+			posicao = i;
+		}
+	}
+	return posicao;
+}
+
