3147e327a4e92c4b121b419addcec99289e89fa8
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-10-24 14:46:18 -0300

    altera index, script e style

--- fundamentos-do-desenvolvimeto-web/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/script.js
+++ fundamentos-do-desenvolvimeto-web/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/exercicios/script.js
@@ -1,6 +1,39 @@
-const divFundo = document.getElementsByClassName('fundo');
-const article = document.querySelector('.conteudo');
+window.onload = function () {
+	let select = document.querySelector('select');
+	select.addEventListener('change', function () {
+		let main = document.querySelector('.conteudo');
+		let corFundo = select.value;
+		main.style.backgroundColor = corFundo;
+		localStorage.setItem('corFundo', corFundo);
+	});
 
+	let input = document.querySelector('input');
+	input.addEventListener('change', function () {
+		p = document.querySelector('.texto-alvo');
+		let fontTamanho = input.value;
+		p.style.fontSize = fontTamanho+'px';
+		localStorage.setItem('fonteTamanho', fontTamanho+'px');
+	});
 
-console.log(window.getComputedStyle(divFundo[0]));
+	let selectTexto = document.querySelector('#texto-cor');
+	selectTexto.addEventListener('change', function () {
+		p = document.querySelector('.texto-alvo');
+		let corFonte = selectTexto.value;
+		p.style.color = corFonte;
+		console.log(corFonte);
+		localStorage.setItem('corFonte', corFonte);
+	});
+	
+
+	let article = document.querySelector('.conteudo');
+	let cor = localStorage.getItem('corFundo');
+	article.style.backgroundColor = cor;
+
+	let p = document.querySelector('.texto-alvo');
+	let tamanhoFonte = localStorage.getItem('fonteTamanho');
+	p.style.fontSize = tamanhoFonte;
+
+	let corFonte = localStorage.getItem('corFonte');
+	p.style.color = corFonte;
+}
 
