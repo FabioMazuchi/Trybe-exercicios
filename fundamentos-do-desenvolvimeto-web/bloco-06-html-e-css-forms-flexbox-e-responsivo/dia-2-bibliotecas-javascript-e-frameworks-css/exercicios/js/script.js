c84bd0a3327c2572982fed55dd0860c627512939
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-11-07 03:23:43 -0300

    finaliza exercicio 6.2

--- fundamentos-do-desenvolvimeto-web/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/exercicios/js/script.js
+++ fundamentos-do-desenvolvimeto-web/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/exercicios/js/script.js
@@ -44,187 +44,134 @@ function criarEstados() {
   }
 }
 
-function getInfoInputs() {
-  const array = [];
-  const nome = [];
-  const value = [];
-  const select = getSelected();
-  const inputs = document.getElementsByTagName('input');
-  
-  for (let i = 0; i < inputs.length; i++) {
-    if(inputs[i].name === 'moradia' && !inputs[i].checked){
-      continue;
-    }
-    nome.push(inputs[i].name);
-    value.push(inputs[i].value); 
-  }
-  array.push(nome);
-  array.push(value);
-  array[0][4] = 'Estado';
-  array[1][4] = select;
-  console.log(array);
-  return array;
-}
+function limpaRadio() {
+  const selectMoradia = document.querySelectorAll("input[name=moradia]");
 
-function exibirInfo() {
-  const info = getInfoInputs();
-  const tamanho = info[0].length;
- 
-  for (let i = 0; i < tamanho; i++) {
-    const divinfo = document.querySelector('#info');
-    const pInfo = document.createElement('p');
-    let nome =info[0][i];
-    let valor = info[1][i];
-    pInfo.innerHTML = nome + ': ' + valor;
-    divinfo.appendChild(pInfo);
-  }
-}
-
-function getSelected() {
-  let selecinado;
-
-  for (let i = 0; i < select.length; i++) {
-    if (select[i].selected === true) {
-      selecinado = select[i].innerHTML;
+  for (let i = 0; i < selectMoradia.length; i += 1) {
+    if (selectMoradia[i].checked === true) {
+      selectMoradia[i].checked = false;
     }
   }
-  return selecinado;
 }
 
+function limpaGeral(e) {
+  e.preventDefault();
+  const inputs = document.getElementsByTagName("input");
+  const textarea = document.querySelector("#resumo");
 
-// function limpaRadio() {
-//   const selectMoradia = document.querySelectorAll("input[name=moradia]");
+  for (let i = 0; i < inputs.length; i += 1) {
+    inputs[i].value = "";
+  }
 
-//   for (let i = 0; i < selectMoradia.length; i += 1) {
-//     if (selectMoradia[i].checked === true) {
-//       selectMoradia[i].checked = false;
-//     }
-//   }
-// }
+  limpaSelecionado();
+  limpaRadio();
 
-// function limpaGeral(e) {
-//   e.preventDefault();
-//   const inputs = document.getElementsByTagName("input");
-//   const textarea = document.querySelector("#resumo");
+  textarea.value = "";
+}
 
-//   for (let i = 0; i < inputs.length; i += 1) {
-//     inputs[i].value = "";
-//   }
+function limpaSelecionado() {
+  for (let s = 0; s < select.length; s += 1) {
+    if (select[s].selected === true) {
+      select[s].selected = false;
+    }
+  }
+}
 
-//   limpaSelecionado();
-//   limpaRadio();
-//   limmparDadosGerados();
-
-//   textarea.value = "";
-// }
-
-// function limpaSelecionado() {
-//   for (let s = 0; s < select.length; s += 1) {
-//     if (select[s].selected === true) {
-//       select[s].selected = false;
-//     }
-//   }
-// }
-
-// function limmparDadosGerados() {
-//   let paragrafos = divInfo.getElementsByTagName("p");
-//   const tamanho = paragrafos.length;
-
-//   for (let i = 0; i <= tamanho; i += 1) {
-//     divInfo.removeChild(divInfo.firstChild);
-//   }
-// }
-
-function enviarDados() {
-  new JustValidate('.js-form', {
-    rules: {
-      nome: {
-        required: true,
-        maxLength: 40,
-      },
-      email: {
-        required: true,
-        maxLength: 50,
-      },
-      cpf: {
-        required: true,
-        maxLength: 11,
-      },
-      endereco: {
-        required: true,
-        maxLength: 28,
-      },
-      cidade: {
-        required: true,
-        maxLength: 28,
-      },
-      estado: {
-        required: true,
-      },
-      moradia: {
-        required: true,
-      },
-      resumo: {
-        required: true,
-        maxLength: 1000,
-      },
-      cargo: {
-        required: true,
-        maxLength: 40,
-      },
-      descricaoCargo: {
-        required: true,
-        maxLength: 500,
-      }
+new JustValidate('.js-form', {
+  rules: {
+    nome: {
+      required: true,
+      maxLength: 40,
     },
-    messages: {
-      nome: {
-        required: 'Campo nome obrigatório.',
-        maxLength: 'Nome máximo com 40 letras.',
-      },
-      email: {
-        required: 'Campo e-mail obrigatório.',
-        maxLength: 'E-mail máximo com 50 letras.',
-      },
-      cpf: {
-        required: 'Campo CPF obrigatório.',
-        maxLength: 'CPF máximo com 11 letras.',
-      },
-      endereco: {
-        required: 'Campo endereço obrigatório',
-        maxLength: 'Endereço máximo com 28 letras.'
-      },
-      cidade: {
-        required: 'Campo cidade obrigatório',
-        maxLength: 'Cidade máxima com 28 letras.',
-      },
-      estado: {
-        required: 'Obrigatório selecionar um estado.',
-      },
-      moradia: {
-        required: 'Obrigatório selecionar um tipo de moradia.'
-      },
-      resumo: {
-        required: 'Campo resumo obrigatório.',
-        maxLength: 'Resumo máximo com 1000 letras.',
-      },
-      cargo: {
-        required: 'Campo cargo obrigatório.',
-        maxLength: 'Cargo máxomo com 40 letras.',
-      },
-      descricaoCargo: {
-        required: 'Campo descrição do cargo obrigatório',
-        maxLength: 'Descrição do cargo máximo com 500 letras.'
-      }
+    email: {
+      required: true,
+      maxLength: 50,
+    },
+    cpf: {
+      required: true,
+      maxLength: 11,
+    },
+    endereco: {
+      required: true,
+      maxLength: 28,
+    },
+    cidade: {
+      required: true,
+      maxLength: 28,
+    },
+    estado: {
+      required: true,
+    },
+    moradia: {
+      required: true,
+    },
+    resumo: {
+      required: true,
+      maxLength: 1000,
+    },
+    cargo: {
+      required: true,
+      maxLength: 40,
+    },
+    descricaoCargo: {
+      required: true,
+      maxLength: 500,
+    },
+    data: {
+      required: true,
     }
-  });
-}
+  },
+  messages: {
+    nome: {
+      required: 'Campo nome obrigatório.',
+      maxLength: 'Nome máximo com 40 letras.',
+    },
+    email: {
+      required: 'Campo e-mail obrigatório.',
+      maxLength: 'E-mail máximo com 50 letras.',
+    },
+    cpf: {
+      required: 'Campo CPF obrigatório.',
+      maxLength: 'CPF máximo com 11 letras.',
+    },
+    endereco: {
+      required: 'Campo endereço obrigatório',
+      maxLength: 'Endereço máximo com 28 letras.'
+    },
+    cidade: {
+      required: 'Campo cidade obrigatório',
+      maxLength: 'Cidade máxima com 28 letras.',
+    },
+    estado: {
+      required: 'Obrigatório selecionar um estado.',
+    },
+    moradia: {
+      required: 'Obrigatório selecionar um tipo de moradia.'
+    },
+    resumo: {
+      required: 'Campo resumo obrigatório.',
+      maxLength: 'Resumo máximo com 1000 letras.',
+    },
+    cargo: {
+      required: 'Campo cargo obrigatório.',
+      maxLength: 'Cargo máxomo com 40 letras.',
+    },
+    descricaoCargo: {
+      required: 'Campo descrição do cargo obrigatório',
+      maxLength: 'Descrição do cargo máximo com 500 letras.'
+    },
+    data: {
+      required: 'Campo data obrigatório.',
+    }
+  },
+  submitHandler : function (form, values) {
+    console.log(form, values);
+  }});
 
 window.onload = function () {
   criarEstados();
-  // const btnlimpar = document.querySelector("#limpar");
-  const btnEnviar = document.querySelector("#enviar");
- 
-  // btnlimpar.addEventListener("click", limpaGeral);
-  btnEnviar.addEventListener("click", enviarDados);
+  const btnlimpar = document.querySelector("#limpar"); 
+
+  btnlimpar.addEventListener("click", limpaGeral);
+
 };
