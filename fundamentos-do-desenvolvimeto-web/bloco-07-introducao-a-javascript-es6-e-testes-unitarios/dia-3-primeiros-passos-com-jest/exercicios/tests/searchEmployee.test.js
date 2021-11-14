80ae5ba8401fd1616971bb9c54a627f87a47e44e
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-11-14 17:49:20 -0300

    adiciona exercicio bonus

--- fundamentos-do-desenvolvimeto-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-com-jest/exercicios/tests/searchEmployee.test.js
+++ fundamentos-do-desenvolvimeto-web/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-com-jest/exercicios/tests/searchEmployee.test.js
@@ -1,2 +1,19 @@
 const searchEmployee = require('../js/searchEmployee');
 
+describe('Testes referentes a função searchEmployee', () => {
+    it('Verifica se a função existe', () => {
+        expect(typeof searchEmployee).toEqual('function');
+    });
+    it('Verifica se ao passar os parâmetros (id, firstName) retorna o valor de firstName', () => {
+       expect(searchEmployee('8579-6', 'firstName')).toEqual('Ana'); 
+       expect(searchEmployee('5569-4', 'firstName')).toEqual('George'); 
+       expect(searchEmployee('4456-4', 'firstName')).toEqual('Leila'); 
+       expect(searchEmployee('1256-4', 'firstName')).toEqual('Linda'); 
+    });
+    it('Verifica se ao passar os parâmetros (id, lastName) retorna o valor de lastName', () => {
+        expect(searchEmployee('8579-6', 'lastName')).toEqual('Gates'); 
+        expect(searchEmployee('5569-4', 'lastName')).toEqual('Jobs'); 
+        expect(searchEmployee('4456-4', 'lastName')).toEqual('Zuckerberg'); 
+        expect(searchEmployee('1256-4', 'lastName')).toEqual('Bezos'); 
+    });
+})
