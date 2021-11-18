60cb1b2b519a1ed9027a67daacad2e30f56a2f1d
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-11-18 13:09:17 -0300

    adidiona dia 8.2

---
+++ fundamentos-do-desenvolvimeto-web/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-forEach-find-some-every-sort/README.md
@@ -0,0 +1,8 @@
+# Dia 2 📆
+
+### Neste dia aprendi:
+
+* Utilizar a função **Array.forEach** para iterar sobre todos os elementos de um array
+* Utilizar a função **Array.find** para encontrar o primeiro elemento de um array que satisfaça a uma condição
+* Utilizar as funções **Array.some** e **Array.every** para testar se os elementos de um array satisfazem a uma condição
+* Utilizar a função **Array.sort** para ordenar arrays de acordo com algum critério
---
+++ fundamentos-do-desenvolvimeto-web/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-forEach-find-some-every-sort/exercicios/exercicio_1.js
@@ -0,0 +1,15 @@
+const newEmployes = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Gerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+		id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+		id3: func('Carla Paiva') // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+  }
+	return employees;
+};
+
+const pessoa = (nomeCompleto) => {
+	const nome = nomeCompleto.split(' ').join('_').toLowerCase();
+	return { nome: nomeCompleto, email: `${nome}@trybe.com`};
+};
+
+console.log(newEmployes(pessoa));
