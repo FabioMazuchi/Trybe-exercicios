1ecef48ee87a3377d90ef530dfa03ae30b9c992c
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-11-19 09:59:21 -0300

    remove arquivos

--- fundamentos-do-desenvolvimeto-web/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/paraFixar.js
+++
@@ -1,9 +0,0 @@
-const acordando = () => 'Acordando!';
-const cafe = () => 'Bora tomar café!';
-const dormir = () => 'Partiu dormir!';
-
-const doingThings = (func) => console.log(func());
-
-doingThings(acordando);
-doingThings(cafe);
-doingThings(dormir);
