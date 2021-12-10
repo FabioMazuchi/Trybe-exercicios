d7d30115bbdb761cc476ca92f4efa2decc3f1b4c
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-12-10 08:22:01 -0300

    adiciona Content ao App

---
+++ desenvolvimento-front-end/bloco-10-introducao-a-react/dia-1-introducao-react/exercicios/my-app-2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+import Content from './Content';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    );
+  }
+}
+export default App;
