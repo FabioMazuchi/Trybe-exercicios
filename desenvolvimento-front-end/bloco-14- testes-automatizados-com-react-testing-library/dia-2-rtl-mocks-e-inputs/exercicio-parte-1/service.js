a3663dd4787cc62228a1a4fd0e4a21c9ed5ddbd5
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-01-27 09:52:19 -0300

    adiciona exercicio 4

--- desenvolvimento-front-end/bloco-14- testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio-parte-1/service.js
+++ desenvolvimento-front-end/bloco-14- testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/exercicio-parte-1/service.js
@@ -1,7 +1,25 @@
-function numeroAleatorio() {
-   return Math.floor(Math.random() * 100);
+const numeroAleatorio = () => Math.floor(Math.random() * 100);
+
+const toMaiusculas = (string) => string.toUpperCase();
+
+const primeiraLetra = (string) => string[0];
+
+const concatStrings = (strA, strB) => `${strA}${strB}`;
+
+function callApi() {
+   const API_URL = 'https://dog.ceo/api/breeds/image/random';
+   return fetch(API_URL)
+   .then(response => response.json())
+   .then(response.ok ?  Promise.resolve(json) : Promise.reject(json));
 }
 
-console.log(numeroAleatorio());
+console.log(callApi());
 
-module.exports = {numeroAleatorio};
+
+module.exports = {
+  numeroAleatorio,
+  toMaiusculas,
+  primeiraLetra,
+  concatStrings,
+  callApi,
+};
