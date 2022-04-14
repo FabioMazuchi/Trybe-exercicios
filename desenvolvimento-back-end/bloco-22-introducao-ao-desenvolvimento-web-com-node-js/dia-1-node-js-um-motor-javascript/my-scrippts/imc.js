4930f19af3f78d53000d3616ebf1b83f526d71f7
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-04-14 08:45:28 -0300

    exerc5

--- desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-um-motor-javascript/my-scrippts/imc.js
+++ desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-um-motor-javascript/my-scrippts/imc.js
@@ -6,9 +6,28 @@ const calculaImc = () => {
 
 	console.log(`Peso: ${peso}, Altura: ${altura}`);
 
-	const imc = (peso / (altura * altura)).toFixed(2);
+  const imc = (peso / Math.pow(altura / 100, 2)).toFixed(2);
 
 	console.log(`IMC: ${imc}`);
+
+	if (imc < 18.5) {
+		console.log('Abaixo do peso (magreza)');
+	}
+	if (imc >= 18.5 && imc < 25) {
+		console.log('Peso normal');
+	}
+	if (imc >= 25 && imc < 30) {
+		console.log('Acima do peso (sobrepeso)');
+	}
+	if (imc >= 30 && imc < 35) {
+		console.log('Obesidade grau I');
+	}
+	if (imc >= 35 && imc < 40) {
+		console.log('Obesidade grau II');
+	}
+	
+	console.log('Obesidade grau III e IV');
+	
 }
 
 calculaImc();
