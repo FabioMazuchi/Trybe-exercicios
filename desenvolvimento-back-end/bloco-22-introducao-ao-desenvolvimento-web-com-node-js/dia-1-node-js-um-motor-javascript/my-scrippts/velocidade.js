5f082401fe58fd200c31e9902e614fd6c3395ab3
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-04-14 08:55:54 -0300

    exerc6

---
+++ desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-1-node-js-um-motor-javascript/my-scrippts/velocidade.js
@@ -0,0 +1,12 @@
+const readline = require('readline-sync');
+
+const calcVelocidade = () => {
+	const dist = readline.questionInt('Qual a distância?(m)');
+	const tempo = readline.questionInt('Qual o tempo?(s)')
+	
+	const vM = (dist / tempo).toFixed(2);
+
+	console.log(`Velocidade média: ${vM} m/s`);
+}
+
+calcVelocidade();
