ff2ddc1000a500da2da4acaae39557a388aa16d5
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-04-22 08:53:32 -0300

    exerc2

--- desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/exercicios/index.js
+++ desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/exercicios/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const userRouter = require('./routers/userRouter');
+const btcRouter = require('./routers/btcRouter');
 
 const app = express();
 app.use(express.json());
 
 app.use('/user', userRouter);
+
+app.use('/btc', btcRouter);
 
 app.listen(3000);
---
+++ desenvolvimento-back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/exercicios/middlewares/authToken.js
@@ -0,0 +1,13 @@
+const authToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (
+    !authorization ||
+    typeof authorization !== "string" ||
+    authorization.length !== 12
+  )
+    return res.status(401).json({ message: "invalid token" });
+	next();	
+};
+
+module.exports = authToken;
