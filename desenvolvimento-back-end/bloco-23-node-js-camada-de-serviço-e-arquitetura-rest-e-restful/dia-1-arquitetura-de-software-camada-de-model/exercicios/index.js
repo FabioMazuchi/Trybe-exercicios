f10e66c80cd242277d6de6907fd8fcbd2ef6cfe1
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-05-03 06:42:09 -0300

    add endpoint post user

--- desenvolvimento-back-end/bloco-23-node-js-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/index.js
+++ desenvolvimento-back-end/bloco-23-node-js-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/index.js
@@ -1,1 +1,9 @@
-console.log('olá');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.post('/user', (req, res) => {
+    res.send('Post');
+});
+
+app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
