6f75bc33fc40aa8d36f4bd622455cd78fc43349d
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-05-04 09:59:58 -0300

    fim

--- desenvolvimento-back-end/bloco-23-node-js-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/index.js
+++ desenvolvimento-back-end/bloco-23-node-js-camada-de-serviço-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exercicios/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const User = require('./models/user');
 const app = express();
 const PORT = 3000;
 
-app.post('/user', (req, res) => {
-    res.send('Post');
+app.use(express.json());
+
+app.get('/user', async (req, res) => {
+	const users = await User.getAll();
+	res.status(200).json(users);
+});
+
+app.get('/user/:id', async (req, res) => {
+	const { id } = req.params;
+	const user = await User.getById(id);
+	res.status(200).json(user);
+});
+
+app.post('/user', async (req, res) => {
+	try {
+		const { first_name, last_name, email, password } = req.body;
+		const user = await User.create(first_name, last_name, email, password);
+		res.status(201).json(user);
+	} catch (e) {
+		return res.status(500).json({ message: e.message})
+	}
+});
+
+app.put('/user/:id', async (req, res) => {
+	const { first_name, last_name, email, password } = req.body;
+	const { id } = req.params;
+	const user = await User.update(id, first_name, last_name, email, password);
+	res.status(200).json(user);
 });
 
 app.listen(PORT, () => console.log(`Ouvindo a porta ${PORT}`));
