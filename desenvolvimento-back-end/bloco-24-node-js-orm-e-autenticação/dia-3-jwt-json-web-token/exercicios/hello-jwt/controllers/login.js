89158993fa4bc5c7a11b48b6bf93b5dd6259f8a7
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-05-25 14:39:06 -0300

    exercicios

--- desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticação/dia-3-jwt-json-web-token/exercicios/hello-jwt/controllers/login.js
+++ desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticação/dia-3-jwt-json-web-token/exercicios/hello-jwt/controllers/login.js
@@ -1,8 +1,12 @@
 const Joi = require('joi');
 
+const Jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
 const validateBody = (body) => 
   Joi.object({
-    username: Joi.string().min(5).alphanum().required()
+    username: Joi.string()
    .messages({
       'string.min': '"username" length must be 5 characters long',
       'string.required': '"username" id required',
@@ -15,9 +19,29 @@ const validateBody = (body) =>
   }).validate(body);
 
 const login = async (req, res, next) => {
+  const { username, password } = req.body;
+  
   const { error } = validateBody(req.body);
 
   if (error) return next(error);
+
+  if (username === 'admin' && password !== 's3nh4S3gur4???') {
+   const err = new Error('Invalid username or password');
+   err.statusCode = 401;
+
+   return next(err);
+  }
+
+  const admin = username === 'admin' && password === 's3nh4S3gur4???';
+  console.log(admin);
+
+  const payload = { username, admin };
+  
+  const token = Jwt.sign(payload, JWT_SECRET, {
+    expiresIn: '1h',
+  });
+
+  res.status(200).json({ token });
 };
 
 module.exports = login;
--- desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticação/dia-3-jwt-json-web-token/exercicios/hello-jwt/index.js
+++ desenvolvimento-back-end/bloco-24-node-js-orm-e-autenticação/dia-3-jwt-json-web-token/exercicios/hello-jwt/index.js
@@ -18,7 +18,7 @@ app.use(
   }),
 );
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/ping', controllers.ping);
