dcdd669de1f24c2369de5601987c3fcd04b66299
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-06-07 20:34:11 -0300

    add

--- desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicios/dist/Directions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Directions;
-(function (Directions) {
-    Directions["ESQUERDA"] = "esquerda";
-    Directions["DIREITA"] = "direita";
-})(Directions || (Directions = {}));
-exports.default = Directions;
