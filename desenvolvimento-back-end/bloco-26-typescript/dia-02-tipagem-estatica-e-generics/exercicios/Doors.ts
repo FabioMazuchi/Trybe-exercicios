1ed5cf835e8b18a7f599cd6aac87f1d4b7689d1a
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-06-07 19:26:02 -0300

    exerc 02

---
+++ desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicios/Doors.ts
@@ -0,0 +1,8 @@
+enum Doors {
+	MOTORISTA = 'do lado do motorista',
+	CARONA = 'do lado do carona',
+	ATRAS_DO_MOTORISTA = 'atrás do motorista',
+	ATRAS_DO_CARONA = 'atrás do carona',
+}
+
+export default Doors
---
+++ desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicios/dist/Directions.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Directions;
+(function (Directions) {
+    Directions["ESQUERDA"] = "esquerda";
+    Directions["DIREITA"] = "direita";
+})(Directions || (Directions = {}));
+exports.default = Directions;
