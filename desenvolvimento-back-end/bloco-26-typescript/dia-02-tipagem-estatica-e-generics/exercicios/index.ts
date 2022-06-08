b50146bb2a59a1a19e9321737da701c6d32ddad1
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-06-08 08:38:08 -0300

    exerc 03

--- desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicios/index.ts
+++ desenvolvimento-back-end/bloco-26-typescript/dia-02-tipagem-estatica-e-generics/exercicios/index.ts
@@ -2,6 +2,7 @@ import Car from './Car';
 import Colors from './Colors';
 import Doors from './Doors'; 
 import Directions from './Directions';
+import Pizza from './Pizza';
 
 const gol = new Car('Volksvagen', Colors.PRATA, 4);
 
@@ -35,4 +36,14 @@ gol.stop();
 gol.openTheDoor(Doors.ATRAS_DO_CARONA);
 gol.closeTheDoor(Doors.ATRAS_DO_CARONA);
 gol.speedUp();
+
+console.log('\n========= PIZZARIA ============');
+const calabreza: Pizza = { flavor: 'Calabreza', slices: 8 }
+const marguerita: Pizza = { flavor: 'Marguerita', slices: 6 }
+const nutela: Pizza = { flavor: 'Nutela', slices: 4 }
+
+console.log(calabreza);
+console.log(marguerita);
+console.log(nutela);
+
 
