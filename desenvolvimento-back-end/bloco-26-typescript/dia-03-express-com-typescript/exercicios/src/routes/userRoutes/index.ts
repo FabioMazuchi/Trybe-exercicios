f0b1031d1e79d103dbb18e048f203e8ddfe919ea
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-06-09 13:36:21 -0300

    add middlware validate user

--- desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/exercicios/src/routes/userRoutes/index.ts
+++ desenvolvimento-back-end/bloco-26-typescript/dia-03-express-com-typescript/exercicios/src/routes/userRoutes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import * as userController from '../../controllers/userController';
+import validateBody from '../../middlewares/userMiddleware';
 
 const userRouter = Router();
 
 userRouter.get('/:id', userController.getById);
 userRouter.get('/', userController.getAll);
+
+userRouter.use(validateBody);
+
 userRouter.post('/', userController.create);
 
 export default userRouter;
