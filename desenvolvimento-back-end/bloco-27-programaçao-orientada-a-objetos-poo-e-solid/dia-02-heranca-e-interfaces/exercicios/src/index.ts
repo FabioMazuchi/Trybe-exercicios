b965cd1e02ea19162a6c8a4f1dd9f1f261b829ee
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-06-21 13:47:49 -0300

    exerc 01

---
+++ desenvolvimento-back-end/bloco-27-programaçao-orientada-a-objetos-poo-e-solid/dia-02-heranca-e-interfaces/exercicios/src/index.ts
@@ -0,0 +1,19 @@
+import { Person } from "./classes/Person";
+
+console.log('=========== PERSON TEST ==============');
+
+const joao = new Person('João', new Date('10-02-2000'));
+const maria = new Person('Maria', new Date('10-03-2010'));
+console.log(joao);
+console.log(maria);
+
+const bob = new Person('Bob', new Date('10-02-2000'));
+const silva = new Person('Silva', new Date('10-03-2022'));
+console.log(bob);
+console.log(silva);
+
+
+
+
+
+
