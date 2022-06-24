647b5fd0a7ed5f31dd4c3d8feadcad3be6da3e6a
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-06-24 15:03:46 -0300

    exerc 05 e 06

--- desenvolvimento-back-end/bloco-27-programaçao-orientada-a-objetos-poo-e-solid/dis-05-solid-principios-L-e-I/exercicios/src/index.ts
+++ desenvolvimento-back-end/bloco-27-programaçao-orientada-a-objetos-poo-e-solid/dis-05-solid-principios-L-e-I/exercicios/src/index.ts
@@ -1,3 +1,4 @@
+import { Airplane } from "./classes/AirPlane";
 import { Car } from "./classes/Car";
 import { CepService } from "./classes/CepService";
 import { FooCepAPI } from "./classes/FooCepAPI";
@@ -34,13 +35,15 @@ async function main() {
 		await cepSvc1.cepByAddress('street foo, betwen bar and baz', 10),
 	);
 
-	console.log('\n=========== CAR ==============');
+	console.log('\n=========== VEÍCULOS ==============');
 	const carFuturist = new FuturistcCar();
 	const car = new Car();
+	const aviao = new Airplane();
 
 	carFuturist.drive();
 	carFuturist.fly();
 	car.drive();
+	aviao.fly();
 }
 
 main();
