4804e0b50f24f948e40be7af0a3cef3627fa858b
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-07-22 14:59:19 -0300

    exercicio 01

---
+++ desenvolvimento-back-end/bloco-29-introducao-ao-mongo-db/dia-05-updates-complexos-arrays/exercicios/exercicio_01.js
@@ -0,0 +1,37 @@
+use("cinema");
+
+// QUERY //
+db.movies.updateOne(
+	{ title: "Batman" },
+	{ $push: { category: "superhero" } }
+);
+
+// RESULTS //
+[
+  {
+    _id: {
+      $oid: "62dae27b66674456e3febee0",
+    },
+    title: "Batman",
+    category: ["action", "adventure", "superhero"],
+    imdbRating: 7.7,
+    budget: 35,
+  },
+  {
+    _id: {
+      $oid: "62dae27b66674456e3febee1",
+    },
+    title: "Godzilla",
+    category: ["action", "adventure", "sci-fi"],
+    imdbRating: 6.6,
+    budget: 1,
+  },
+  {
+    _id: {
+      $oid: "62dae27b66674456e3febee2",
+    },
+    title: "Home Alone",
+    category: ["family", "comedy"],
+    imdbRating: 7.4,
+  },
+];
