4175017bc57be1fe0db0ef0deee38dcc53618e1e
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-07-28 20:29:18 -0300

    exercicio 08

--- desenvolvimento-back-end/bloco-30-mongodb-com-node-js-e-poo/dia-01-mongoose-e-arquitetura-msc-camada-model/exercicios/src/tests/mocks/lensMock.ts
+++ desenvolvimento-back-end/bloco-30-mongodb-com-node-js-e-poo/dia-01-mongoose-e-arquitetura-msc-camada-model/exercicios/src/tests/mocks/lensMock.ts
@@ -13,4 +13,11 @@ const lensMockWithId: ILens & { _id: string } = {
 	blueLightFilter: true,
 };
 
-export { lensMock, lensMockWithId };
+const lensMockForChangeWithId:ILens & { _id:string } = {
+	_id: '62cf1fc6498565d94eba52cd',
+	degree: 9,
+  antiGlare: false,
+  blueLightFilter: true,
+};
+
+export { lensMock, lensMockWithId, lensMockForChangeWithId };
--- desenvolvimento-back-end/bloco-30-mongodb-com-node-js-e-poo/dia-01-mongoose-e-arquitetura-msc-camada-model/exercicios/src/tests/unit/models/lens.test.ts
+++ desenvolvimento-back-end/bloco-30-mongodb-com-node-js-e-poo/dia-01-mongoose-e-arquitetura-msc-camada-model/exercicios/src/tests/unit/models/lens.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { Model } from 'mongoose';
 import sinon from 'sinon';
 import LensModel from '../../../models/Lens';
-import { lensMock, lensMockWithId } from '../../mocks/lensMock';
+import { lensMock, lensMockForChangeWithId, lensMockWithId } from '../../mocks/lensMock';
 
 describe('Lens Model', () => {
 	const lensModel = new LensModel();
@@ -11,6 +11,7 @@ describe('Lens Model', () => {
 		sinon.stub(Model, 'create').resolves(lensMockWithId);
 		sinon.stub(Model, 'findOne').resolves(lensMockWithId);
 		sinon.stub(Model, 'find').resolves([lensMockWithId]);
+		sinon.stub(Model, 'findByIdAndDelete').resolves(lensMockForChangeWithId);
 	});
 
 	after(() => {
@@ -47,5 +48,20 @@ describe('Lens Model', () => {
 	
 			expect(newLens).to.be.deep.equal([lensMockWithId]);
 		});
+	});
+
+	describe('deleting a lens', () => {
+		it('successful deletions', async () => {
+			const lensDeleted = await lensModel.destroy('62cf1fc6498565d94eba52cd');
+			expect(lensDeleted).to.be.deep.equal(lensMockForChangeWithId); 
+		});
+
+		it('_id not found', async () => {
+			try {
+				await lensModel.destroy('ERRADO');
+			} catch (error: any) {
+				expect(error.message).to.be.eq('InvalidMongoId');
+			}
+		})
 	});
 });
