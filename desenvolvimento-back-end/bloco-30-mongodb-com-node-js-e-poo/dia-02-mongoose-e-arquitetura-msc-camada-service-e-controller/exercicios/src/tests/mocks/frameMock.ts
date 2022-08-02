037f439b11df11be493b88a0635b73d22d6365fa
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-08-02 20:36:11 -0300

    exercicio 02

--- desenvolvimento-back-end/bloco-30-mongodb-com-node-js-e-poo/dia-02-mongoose-e-arquitetura-msc-camada-service-e-controller/exercicios/src/tests/mocks/frameMock.ts
+++ desenvolvimento-back-end/bloco-30-mongodb-com-node-js-e-poo/dia-02-mongoose-e-arquitetura-msc-camada-service-e-controller/exercicios/src/tests/mocks/frameMock.ts
@@ -11,10 +11,18 @@ const frameMockWithId: IFrame & { _id: string} = {
 	color: 'Amarelo'
 };
 
+const allFrameMock: IFrame[] & { _id: string }[] = [
+	{
+		_id: '62cf1fc6498565d94eba52cd',
+		material: 'Ouro',
+		color: 'Amarelo'
+	},
+];
+
 const frameMockForChangeWithId:IFrame & { _id:string } = {
 	_id: '62cf1fc6498565d94eba52cd',
 	material: 'Ouro com adamantium',
 	color: 'ouro brilhante',
 };
 
-export { frameMock, frameMockWithId, frameMockForChangeWithId };
+export { frameMock, frameMockWithId, frameMockForChangeWithId, allFrameMock };
--- desenvolvimento-back-end/bloco-30-mongodb-com-node-js-e-poo/dia-02-mongoose-e-arquitetura-msc-camada-service-e-controller/exercicios/src/tests/unit/models/frame.test.ts
+++ desenvolvimento-back-end/bloco-30-mongodb-com-node-js-e-poo/dia-02-mongoose-e-arquitetura-msc-camada-service-e-controller/exercicios/src/tests/unit/models/frame.test.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { Model } from 'mongoose';
 import sinon from 'sinon';
 import FrameModel from '../../../models/Frame';
-import { frameMock, frameMockForChangeWithId, frameMockWithId } from '../../mocks/frameMock';
+import { allFrameMock, frameMock, frameMockForChangeWithId, frameMockWithId } from '../../mocks/frameMock';
 
 describe('Frame Model', () => {
 	const frameModel = new FrameModel();
@@ -10,7 +10,7 @@ describe('Frame Model', () => {
 	before(() => {
 		sinon.stub(Model, 'create').resolves(frameMockWithId);
 		sinon.stub(Model, 'findOne').resolves(frameMockWithId);
-		sinon.stub(Model, 'find').resolves([frameMockWithId]);
+		sinon.stub(Model, 'find').resolves(allFrameMock);
 		sinon.stub(Model, 'findByIdAndDelete').resolves(frameMockForChangeWithId);
 	});
 
@@ -18,14 +18,14 @@ describe('Frame Model', () => {
 		sinon.restore();
 	});
 
-	describe('creating a frame', () => {
+	describe.skip('creating a frame', () => {
 		it('successfully created', async () => {
 			const newFrame = await frameModel.create(frameMock);
 			expect(newFrame).to.be.deep.equal(frameMockWithId);
 		});
 	});
 
-	describe('searching a frame', () => {
+	describe.skip('searching a frame', () => {
 		it('successfully found', async () => {
 			const framesFound = await frameModel.readOne('62cf1fc6498565d94eba52cd');
 			expect(framesFound).to.be.deep.equal(frameMockWithId);
@@ -43,11 +43,11 @@ describe('Frame Model', () => {
 	describe('read a frame', () => {
 		it('successfully read', async () => {
 			const frames = await frameModel.read();
-			expect(frames).to.be.deep.equal([frameMockWithId]);
+			expect(frames).to.be.deep.equal(allFrameMock);
 		});
 	});
 
-	describe('deleting a frame', () => {
+	describe.skip('deleting a frame', () => {
 		it('successful deletion', async () => {
 			const frameDeleted = await frameModel.destroy('62cf1fc6498565d94eba52cd');
 			expect(frameDeleted).to.be.deep.equal(frameMockForChangeWithId);
