7e7abf3459e58a614fd5a55ee13d9229d599ff77
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-10 10:24:24 -0300

    exercicio 03

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-03-testes/exercicios/test_fizzbuzz.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-03-testes/exercicios/test_fizzbuzz.py
@@ -2,7 +2,7 @@ from funcoes import fizzbuzz
 import pytest
 
 
-def test_fizz_buxx_error():
+def test_error():
     with pytest.raises(TypeError):
         fizzbuzz("3")
 
@@ -11,13 +11,13 @@ def test_fizz_buzz():
     assert fizzbuzz(2) == [1, 2]
 
 
-def test_fizz_buzz_return_fizz():
+def test_return_fizz():
     assert fizzbuzz(3) == [1, 2, "Fizz"]
 
 
-def test_fizz_buzz_return_buzz():
+def test_return_buzz():
     assert fizzbuzz(5) == [1, 2, "Fizz", 4, "Buzz"]
 
 
-def test_fizz_buzz_return_fizzbuzz():
+def test_return_fizzbuzz():
     assert fizzbuzz(15)[-1] == "FizzBuzz"
