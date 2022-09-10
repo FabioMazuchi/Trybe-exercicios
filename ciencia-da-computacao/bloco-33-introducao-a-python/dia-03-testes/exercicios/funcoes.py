9fd9845541906a61e626f43dc855a32e960f37d5
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-10 10:24:03 -0300

    exercicio 03

--- ciencia-da-computacao/bloco-33-introducao-a-python/dia-03-testes/exercicios/funcoes.py
+++ ciencia-da-computacao/bloco-33-introducao-a-python/dia-03-testes/exercicios/funcoes.py
@@ -11,3 +11,38 @@ def fizzbuzz(numero):
             else:
                 list.append(n + 1)
     return list
+
+
+def validate_email(email):
+    i = 0
+    if not email[i].isalpha():
+        raise ValueError("Email deve iniciar com letras")
+
+    while email[i] != "@" and i < len(email):
+        result = email[i]
+        if (
+            not result.isalpha()
+            and not result.isdigit()
+            and result not in ("_", "-")
+        ):
+            raise ValueError(
+                "O nome de usuário deve conter apenas letras, "
+                "dígitos, traços ou underscors"
+            )
+        i += 1
+    i += 1
+
+    while email[i] != "." and i < len(email):
+        result = email[i]
+        if not result.isalpha() and not result.isdigit():
+            raise ValueError("O site deve conter apenas letras e dígitos")
+        i += 1
+    i += 1
+
+    count = 0
+    while i < len(email):
+        count += 1
+        i += 1
+    if count > 3:
+        raise ValueError("Valor maximo da extensão é 3")
+    return None
