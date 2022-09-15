d741acc210ecbbad109bbcf8ac053f8111655cca
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-15 20:03:35 -0300

    exercicio 04

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-01-poo-em-python/exercicios/app.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-01-poo-em-python/exercicios/app.py
@@ -3,6 +3,9 @@ from classes.Quadrado import Quadrado
 from classes.Estatisticas import Estatisticas
 from classes.Tv import Tv
 from classes.Circulo import Circulo
+from classes.LogEmTela import LogEmTela
+from classes.Log import Log
+from classes.LogEmArquivo import LogEmArquivo
 
 numbers = [1, 4, 5, 8, 9, 5.7, 8, 10]
 
@@ -31,4 +34,9 @@ print(f"Perímetro do retângulo = {retangulo.perimetro():.1f}m\n")
 
 circulo = Circulo(10)
 print(f"Área do circulo = {circulo.area():.1f}m²")
-print(f"Perímetro do circulo = {circulo.perimetro():.1f}m")
+print(f"Perímetro do circulo = {circulo.perimetro():.1f}m\n")
+
+log_em_tela = LogEmTela()
+log_em_arquivo = LogEmArquivo()
+log = Log([log_em_tela, log_em_arquivo])
+log.erro("ZeroDivisionError: division by zero")
