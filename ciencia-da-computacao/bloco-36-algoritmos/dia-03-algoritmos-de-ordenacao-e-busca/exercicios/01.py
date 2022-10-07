80c61290dcaf2a3673fad08e7a00a0305345955c
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-07 11:35:37 -0300

    exercicio 01

---
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/exercicios/01.py
@@ -0,0 +1,6 @@
+animais = ["zebra", "macaco", "elefante", "arara", "javali"]
+
+# 1ª iteração
+["macaco", "elefante", "arara", "javali", "zebra",]
+# 2ª iteração
+["elefante", "arara", "javali", "macaco", "zebra"]
