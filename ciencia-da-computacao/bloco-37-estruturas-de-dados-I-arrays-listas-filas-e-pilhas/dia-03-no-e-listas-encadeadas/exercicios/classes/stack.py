3e63d732cc7a2543cc0c4f9d39cce8325af9ccec
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-17 11:51:05 -0300

    feat: push in Stack

--- ciencia-da-computacao/bloco-37-estruturas-de-dados-I-arrays-listas-filas-e-pilhas/dia-03-no-e-listas-encadeadas/exercicios/classes/stack.py
+++ ciencia-da-computacao/bloco-37-estruturas-de-dados-I-arrays-listas-filas-e-pilhas/dia-03-no-e-listas-encadeadas/exercicios/classes/stack.py
@@ -4,13 +4,21 @@ from rich import print
 
 class Stack:
     def __init__(self):
-        self.__data = LinkedList()
+        self.data = LinkedList()
 
     def is_empty(self):
-        return not len(self.__data)
+        return not len(self.data)
+
+    def push(self, value):
+        self.data.insert_last(value)
 
 
 if __name__ == "__main__":
     stack = Stack()
 
     print(stack.is_empty())
+
+    stack.push(5)
+
+    print(stack.is_empty())
+    print(stack.data)
