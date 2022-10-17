3db18fa2b5526fa528a88d339ef38976e02fe360
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-10-17 13:44:50 -0300

    feat: enqueue in Queue

--- ciencia-da-computacao/bloco-37-estruturas-de-dados-I-arrays-listas-filas-e-pilhas/dia-03-no-e-listas-encadeadas/exercicios/classes/queue.py
+++ ciencia-da-computacao/bloco-37-estruturas-de-dados-I-arrays-listas-filas-e-pilhas/dia-03-no-e-listas-encadeadas/exercicios/classes/queue.py
@@ -9,8 +9,14 @@ class Queue:
     def is_empty(self):
         return not len(self.data)
 
+    def enqueue(self, value):
+        self.data.insert_last(value)
+
 
 if __name__ == "__main__":
     queue = Queue()
 
     print(queue.is_empty())
+
+    queue.enqueue(3)
+    print(queue.data)
